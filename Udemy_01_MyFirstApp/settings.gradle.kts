bfec9b2af60d51ea69ffb187c899971f444fcf7b
Dani Costas <145155375+danicostas-xyz@users.noreply.github.com>
2024-12-18 12:46:26 +0100

    Add Udemy_01_myFirstApp

---
+++ Udemy_01_MyFirstApp/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Udemy_01_MyFirstApp"
+include(":app")
+ 
